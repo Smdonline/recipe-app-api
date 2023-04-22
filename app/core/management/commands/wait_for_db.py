189fc2a2ce7ea00bf59406928467135a31665725
socia <socianudaniel@gmail.com>
2023-04-23 01:41:18 +0200

    aded postgresql settings

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,11 +2,25 @@
 Dajngo command to wait for databade to bee available
 
 """
-
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Dajngo command to wait for database"""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command"""
+        self.stdout.write("Wait for database .....")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(database=['default'])
+                db_up = True
+            except (Psycopg2Error,  OperationalError):
+                self.stdout.write('Database not available, waiting 1 seccond...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
+
