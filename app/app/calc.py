7495272283499d37e51f6e7d6a1e81cdc9376df1
Smdonline <4cVVgJN5dZYUhtM>
2023-04-22 12:15:16 +0200

    add test for wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator function
+"""
+
+def add(x, y):
+    """ add two numbers test porpuses """
+    return x + y
+
+def subtract(x, y):
+    """Subtract two numbers"""
+    return y - x
+
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Dajngo command to wait for databade to bee available
+
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Dajngo command to wait for database"""
+
+    def handle(self, *args, **options):
+        pass
